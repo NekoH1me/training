d5e84dcecb90229da53fcf17feebb9c31cc316dc
Daria <34098921+NekoH1me@users.noreply.github.com>
2018-01-26 10:53:02 +0400

    ~

---
+++ Task9 (js)/Task11.js
@@ -0,0 +1,13 @@
+  console.log('Task11: Day and month');
+
+  function getNames(date) {
+    if (typeof date === 'object') {
+      return date.toLocaleString("ja-JP-u-ca-japanese", {month: 'long'}) 
+      + '(' + date.toLocaleString("en", {month: 'long'}) + ') '
+      + date.toLocaleString("ja-JP-u-ca-japanese", {weekday: 'long'})
+      + '(' + date.toLocaleString("en", {weekday: 'long'}) + ')'
+    }
+  }
+
+  console.log(getNames(new Date()));
+  
