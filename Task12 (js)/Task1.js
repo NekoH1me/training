4b568340cc44cc2b7108a726b5aef2bd96453036
NekoH1me <basreville97@mail.ru>
2018-02-16 18:10:38 +0400

    ~

--- Task12 (js)/Task1.js
+++ Task12 (js)/Task1.js
@@ -1,28 +1,7 @@
+var Ball = require('.../Task1/Ball.js');
+var SomeBallDecorator = require('.../Task1/SomeBallColor.js');
+var LineBallDecorator = require('.../Task1/SomeBallLine.js');
+
 console.log('Task1');
-
-  function Ball(ball) {
-    this.ball = ball;
-  }
-
-  Ball.prototype.getDescription = function () {
-    return 'ball';
-  }
-
-  function SomeBallColor(ball) {
-    this.ball = ball;
-  }
-
-  SomeBallColor.prototype.getDescription = function () {
-    return ('red ' + this.ball.getDescription());
-  }
-
-  function SomeBallLine(ball) {
-    this.ball = ball;
-  }
-
-  SomeBallLine.prototype.getDescription = function () {
-    return (this.ball.getDescription() + ' with lines');
-  }
-
 console.log(new Ball().getDescription ()); //ball
 console.log(new SomeBallLine(new SomeBallColor(new Ball())).getDescription ()); // red ball with lines
