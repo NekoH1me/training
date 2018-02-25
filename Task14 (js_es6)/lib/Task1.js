973a9569bef82037defa0c56d65bc9d8a1ad9045
Daria <34098921+NekoH1me@users.noreply.github.com>
2018-02-25 14:50:25 +0400

    Delete Task1.js

--- Task14 (js_es6)/lib/Task1.js
+++
@@ -1,7 +0,0 @@
-var Ball = require('../Task1/Ball.js');
-var SomeBallColor = require('../Task1/SomeBallColor.js');
-var SomeBallLine = require('../Task1/SomeBallLine.js');
-
-console.log('Task1');
-console.log(new Ball().getDescription ()); //ball
-console.log(new SomeBallLine(new SomeBallColor(new Ball())).getDescription ()); // red ball with lines
